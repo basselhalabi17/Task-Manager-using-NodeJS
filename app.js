28786389bad8e37d80fb104e60df521c09c38e09
basselhalabi17 <45502245+basselhalabi17@users.noreply.github.com>
2022-09-14 16:03:21 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const start = async () => {
     try{
         await connectDB(process.env.MONGO_URI)
         app.listen(port,()=>{
-            console.log('Home')
+            console.log('Homesss')
         })
     }
     catch(error){
